9d098cc843745fe56293963bd14c7bbf125cbc40
I <ippolit.mufteev@r-x.team>
2025-02-23 15:01:38 +0500

    apppend errors and atomic flag stoppped

--- pool.go
+++ pool.go
@@ -1,7 +1,15 @@
 package workerpool
 
+import (
+	"errors"
+	"sync/atomic"
+)
+
+type Task func()
+
 type Pool struct {
 	collector chan Task
+	isStopped atomic.Bool
 
 	Tasks   []*Task
 	Workers []*worker
@@ -9,12 +17,23 @@ type Pool struct {
 	workerCount int
 }
 
-func NewPool(workerCount, collectorCount int) *Pool {
+var (
+	ErrTaskNil         = errors.New("task is nil")
+	ErrPoolStopped     = errors.New("pool is stopped")
+	ErrInvalidPoolSize = errors.New("workerCount and collectorCount must be greater than 0")
+)
+
+func NewPool(workerCount, collectorCount int) (*Pool, error) {
+	if workerCount <= 0 || collectorCount <= 0 {
+		return nil, ErrInvalidPoolSize
+	}
+
 	return &Pool{
+		isStopped:   atomic.Bool{},
 		workerCount: workerCount,
 		Workers:     make([]*worker, workerCount),
 		collector:   make(chan Task, collectorCount),
-	}
+	}, nil
 }
 
 func (p *Pool) AddTask(t Task) error {
@@ -41,6 +60,8 @@ func (p *Pool) RunBackground() {
 }
 
 func (p *Pool) Stop() {
+	p.isStopped.Store(true)
+
 	for i := range p.workerCount {
 		p.Workers[i].stop()
 	}
